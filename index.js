a1d286deb4deae15a82cb7b21c9314940614cb61
Wulan <widyawulangit19@gmail.com>
2025-07-09 01:52:01 +0700

    menambahkan endpoin get archive universal

--- index.js
+++ index.js
@@ -4929,6 +4929,18 @@ app.get('/api/marketing-designs-null', async(req,res)=>{
         res.status(500).json({ message: 'Internal server error' });
     }
 })
+
+//ARCHIVE UNIVERSAL
+//1. get all data archive
+app.get('/api/archive-data', async (req, res) => {
+  try {
+    const result = await client.query(`SELECT * FROM archive_universal ORDER BY archived_at DESC`);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error('Error fetching archive:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
   
 // ARCHIVE DATA 
 //1. get all workspace archive
