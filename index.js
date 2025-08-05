cdeb55e23f7ebf49f06eeaf5e8f6fb374e995769
Wulan <widyawulangit19@gmail.com>
2025-08-05 19:56:43 +0700

    perubahan pada endpoin update

--- index.js
+++ index.js
@@ -504,7 +504,7 @@ app.put('/api/user-setting/:userId', async (req, res) => {
     `, [name, nomor, divisi, jabatan, userId]);
 
     // Cek apakah user_profil sudah ada
-    const existingProfil = await client.query(`
+    const existingProfil = await client.query(` index.js
       SELECT profil.id, profil.photo_url 
       FROM user_profil 
       JOIN profil ON user_profil.profil_id = profil.id
