461094f4ba2c079c5623fd9fe317201f2e30a061
Wulan <widyawulangit19@gmail.com>
2025-08-30 02:47:00 +0700

    perubahan pada halaman page profil

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -137,7 +137,8 @@ const Profile=()=> {
             case 'personal':
                 return <div className="fade"><PersonalInformation fetchUserSettingData={fetchUserSettingData} userData={userData} userId={userId}/></div>;
             case 'activity':
-                return <div className="fade"><div className='activity'><ActivityPage userId={userId}/></div></div>;
+                // return <div className="fade"><div className='activity'><ActivityPage userId={userId}/></div></div>;
+                                return <div className="fade"><div className='activity'><h4>SOON</h4></div></div>;
             case 'notification':
                 return <div className="fade"><div className="notif"><PersonalNotification userId={userId}/></div></div>;
             case 'security':
