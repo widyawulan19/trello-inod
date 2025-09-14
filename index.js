17d1047ae9fed7f7d05bd71f288d9ce934014794
Wulan <widyawulangit19@gmail.com>
2025-09-15 01:47:33 +0700

    perubahan

--- index.js
+++ index.js
@@ -6372,9 +6372,10 @@ app.post("/api/marketing-design/joined", async (req, res) => {
             reference,
             project_type_id,
             style_id,
-            status_project_id
+            status_project_id,
+            create_at
         )
-        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22)
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22, NOW())
         RETURNING *;
 
             `,
