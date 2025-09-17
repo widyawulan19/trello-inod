d8b3b4b895786985d27e512cb2f27421e16719c1
Wulan <widyawulangit19@gmail.com>
2025-09-17 14:52:40 +0700

    perubahan

--- frontend/src/pages/NewCardDetail.jsx
+++ frontend/src/pages/NewCardDetail.jsx
@@ -35,7 +35,7 @@ import UploadFile from '../modals/UploadFile';
 import NewRoomChat from '../fitur/NewRoomChat';
 import { FaXmark } from 'react-icons/fa6';
 import CardDescription from '../modals/CardDesctiption';
-import ReactQuill from "react-quill-new";   // ✅ beda disini
+import ReactQuill from 'react-quill-new';
 // import "react-quill-new/dist/quill.snow.css"; // ✅ CSS dari react-quill-new
 import "quill/dist/quill.snow.css";
 
