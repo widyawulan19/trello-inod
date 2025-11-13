aaa26c5df11634375cc2f804d9703e3569523a28
Wulan <widyawulangit19@gmail.com>
2025-11-13 12:42:02 +0700

    penambahan url hostinger

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ app.use(express.json());
 app.use(cors({
     // origin: "*",
     // origin: ["http://localhost:3000", "https://inodstudiomanagement.vercel.app"],
-    origin: ["http://localhost:3000", "https://trello-inod.vercel.app"],
+    origin: ["http://localhost:3000", "https://trello-inod.vercel.app", "https://lightcyan-rhinoceros-611327.hostingersite.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
