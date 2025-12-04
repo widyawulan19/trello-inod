18e753fbb8ac959d676f8d2300dbba5e43c4dbd9
Wulan <widyawulangit19@gmail.com>
2025-12-04 18:29:35 +0700

    perubahan

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ dayjs.extend(timezone);
 dayjs.locale("id");
 
 
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
+
+
+// batas besar di multer (WAJIB)
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 200 * 1024 * 1024 } // 200 MB
+});
 
 
 // =======================
