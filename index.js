151c1b7ea237df83f0d0ce16cd74b1eb4e7958ae
Wulan <widyawulangit19@gmail.com>
2025-12-05 15:14:13 +0700

    perubahan

--- index.js
+++ index.js
@@ -211,8 +211,13 @@ module.exports = { generateMarketingDesignNumbers };
 //TOP
 dotenv.config();
 
+
 const app = express();
 app.use(express.json());
+
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
+
 
 app.use(cors({
     // origin: "*",
