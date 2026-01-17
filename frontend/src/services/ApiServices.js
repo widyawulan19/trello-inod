ce327fefb130072348c72e462a961a7f8d93f262
Wulan <widyawulangit19@gmail.com>
2026-01-17 23:36:22 +0700

    perubahan

--- frontend/src/services/ApiServices.js
+++ frontend/src/services/ApiServices.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const API_URL = 'https://trello-inod-production.up.railway.app/api'
-// const API_URL = 'https://trello-inod-production.up.railway.app/api';
+// REACT_APP_API_URL=https://inod-backend.up.railway.app/api
 
 const API_URL = process.env.REACT_APP_API_URL;
 
