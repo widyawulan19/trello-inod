f645ab9724259177091b15ad690d5a2f80dd9f13
Wulan <widyawulangit19@gmail.com>
2026-02-12 04:40:35 +0700

    perubahan

--- connection.js
+++ connection.js
@@ -1,37 +1,43 @@
-// require('dotenv').config();
-// const { Client } = require('pg');
-
-// // Buat client pakai DATABASE_URL
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false, // Wajib untuk koneksi Railway
-//   },
-// });
-
-// client.connect()
-//   .then(() => console.log('Connected to Railway Postgres'))
-//   .catch(err => console.error('Connection error', err.stack));
-
-// module.exports = client;
-
-
 require('dotenv').config();
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
-const pool = new Pool({
+// Buat client pakai DATABASE_URL
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false, // Wajib untuk koneksi Railway
+  },
 });
 
-pool.on('connect', () => {
-  console.log('✅ Connected to Postgres');
-});
+client.connect()
+  .then(() => console.log('Connected to Railway Postgres'))
+  .catch(err => console.error('Connection error', err.stack));
 
-pool.on('error', (err) => {
-  console.error('❌ DB error:', err);
-});
+module.exports = client;
 
-module.exports = pool;
+
+// require('dotenv').config();
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// pool.on('connect', () => {
+//   console.log('✅ Connected to Postgres');
+// });
+
+// pool.on('error', (err) => {
+//   console.error('❌ DB error:', err);
+// });
+
+
+// pool.connect()
+//   .then(() => console.log('Connected to Railway Postgres'))
+//   .catch(err => console.error('Connection error', err));
+
+
+// module.exports = pool;
