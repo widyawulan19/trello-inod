644180ddcea1f91513ecb0ff5f5709c950b5c12c
Wulan <widyawulangit19@gmail.com>
2026-02-12 05:18:40 +0700

    perubahan

--- connection.js
+++ connection.js
@@ -31,7 +31,7 @@ client.connect()
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
